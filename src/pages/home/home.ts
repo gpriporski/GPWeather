69433997334c33769691f213069e55f1efa084c1
Georgi Priporski <priporski@gmail.com>
2018-12-11 17:57:35 +0200

    home.ts changed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,43 +1,41 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { WeatherProvider } from '../../providers/weather/weather';
-import { Storage } from '@ionic/storage';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { WeatherProvider } from "../../providers/weather/weather";
+import { Storage } from "@ionic/storage";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
-  weather:any;
+  weather: any;
   location: {
-    city:string,
-    code:string
-  }
+    city: string;
+    code: string;
+  };
 
   constructor(
     public navCtrl: NavController,
-    private weatherProvider:WeatherProvider,
-    private storage:Storage) {
-
-  }
+    private weatherProvider: WeatherProvider,
+    private storage: Storage
+  ) {}
 
   ionViewWillEnter() {
-    this.storage.get('location').then((val) => {
-      if(val != null) {
+    this.storage.get("location").then(val => {
+      if (val != null) {
         this.location = JSON.parse(val);
       } else {
         this.location = {
-          city: 'Sofia',
-          code: 'BG'
-        }
+          city: "Sofia",
+          code: "BG"
+        };
       }
 
-      this.weatherProvider.getWeather(this.location.city, this.location.code)
+      this.weatherProvider
+        .getWeather(this.location.city, this.location.code)
         .subscribe(weather => {
           this.weather = weather;
         });
     });
   }
-
 }
